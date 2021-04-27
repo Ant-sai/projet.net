4770b03c1e853193bd8ebceec9276f43ca178ca4
Damien-Ar <damien.Aranda42@gmail.com>
2021-04-27 10:19:08 +0200

    api fonctionnelle

---
+++ APIWeb demo/Controllers/SonnetteController.cs
@@ -0,0 +1,28 @@
+﻿using APIWeb_demo.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIWeb_demo.Models;
+
+namespace APIWeb_demo.Controllers
+{
+    [ApiController]
+    [Route("api/Sonnette")]
+    public class SonnetteController : Controller
+    {
+        private readonly IHubContext<NotifHub> _notificationHubContext;
+        public SonnetteController(IHubContext<NotifHub> notificationHubContext)
+        {
+            _notificationHubContext = notificationHubContext;
+        }
+
+        [HttpPost]
+        public async void Index(Sonnette model)
+        {
+            await _notificationHubContext.Clients.All.SendAsync("ReceiveNotif",  model.state);
+        }
+    }
+}
---
+++ APIWeb demo/Models/Sonnette.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIWeb_demo.Models
+{
+    public class Sonnette
+    {
+        public Boolean state { get; set; }
+    }
+}
