c0fc7173a3951fb4ccd15bb04fdfcf7a6d19197d
Antoine <antoine.saintlouis@gmail.com>
2021-05-04 14:08:27 +0200

    that working

--- APIWeb demo/Startup.cs
+++ APIWeb demo/Startup.cs
@@ -46,6 +46,12 @@ namespace APIWeb_demo
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIWeb_demo v1"));
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
 
