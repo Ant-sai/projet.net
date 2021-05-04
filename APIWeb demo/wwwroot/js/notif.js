5ace095e1de5f77ad5679b3221a2c4ec510a4188
Antoine <antoine.saintlouis@gmail.com>
2021-05-04 16:05:59 +0200

    serveur qui marche

--- APIWeb demo/wwwroot/js/notif.js
+++ APIWeb demo/wwwroot/js/notif.js
@@ -15,6 +15,9 @@ connection.on("ReceiveNotif", function (message) {
         console.log("sonnette active")
         li.textContent = `La sonnette a été appuyé à ${date.toLocaleTimeString()}`;
         document.getElementById("sonnette").classList.add("sonnette-active")
+        if (currentState < 5) {
+            changeState(currentState + 1)
+        }
     }
     else {
         console.log("sonnette inactive")
@@ -30,3 +33,35 @@ connection.start()
     .catch(function (err) {
         return console.error(err.toString());
     });
+
+
+/* card alerte */
+
+let cardAlert = document.getElementById("alerte-sonnette")
+let textAlert = document.getElementById("text-alert")
+let currentState = 0
+let statePhrase = [
+    "Nobody's here",
+    "Toc toc",
+    "Someone's waiting",
+    "Hey, hurry up",
+    "That's not realy nice to leave someone outside",
+    "M*********r, will you open this g*d d**n door !!!"
+]
+
+function changeState(newState) {
+    console.log("change state attempt " + newState)
+    console.log(cardAlert)
+    console.log("old state :" + currentState)
+    cardAlert.classList.remove(`alerte-state-${currentState}`)
+    cardAlert.classList.add(`alerte-state-${newState}`)
+    textAlert.innerText = statePhrase[newState]
+    currentState = newState
+    console.log("new state :" + currentState)
+}
+
+document.getElementById("open").addEventListener("click", () => {
+    changeState(0)
+})
+
+changeState(0)
